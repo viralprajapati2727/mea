e1aca01071cab9952d9efb7201aa15e9885c22d0
Nisha Prajapati <nishaprajapatilive2727@gmail.com>
2020-11-27 22:22:37 +0530

    Register Page Radio button add

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,12 +11,21 @@
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
-
+                        <div class="form-group row">
+                            <div class="form-radio-group">
+                                <label class="radio-inline active">
+                                    <i class="flaticon-man"></i>
+                                    <input type="radio" class="" name="user_type" value="2" checked="">Candidate</label>
+                                <label class="radio-inline">
+                                    <i class="flaticon-company"></i>
+                                    <input type="radio" class="" name="user_type" value="3">Company</label>
+                            </div>   
+                        </div>
                         <div class="form-group row">
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
 
                             <div class="col-md-12">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" placeholder="Name" autofocus>
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}"  autocomplete="name" placeholder="Name" autofocus>
 
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
@@ -30,7 +39,7 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-12">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Email Id">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}"  autocomplete="email" placeholder="Email Id">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -44,7 +53,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-12">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="Password">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password"  autocomplete="new-password" placeholder="Password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
