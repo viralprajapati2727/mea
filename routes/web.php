7fcf08614f2cbc85fa967c07a2217c893ea63758
Nisha Prajapati <nishaprajapatilive2727@gmail.com>
2020-11-28 22:54:00 +0530

    Starting Business Ideas page

--- routes/web.php
+++ routes/web.php
@@ -88,3 +88,6 @@ Route::get('html-profile',function (){
 Route::get('html-edit-profile',function (){
     return view('html.edit-profile');
 });
+Route::get('html-business-ideas',function (){
+    return view('html.business-ideas');
+});
