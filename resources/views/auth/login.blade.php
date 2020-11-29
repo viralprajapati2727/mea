fdf36473c7e5c9c04b92219c3ef3898f8aec3f4e
Nisha Prajapati <nishaprajapatilive2727@gmail.com>
2020-11-29 23:13:31 +0530

    Job-portal-page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -57,12 +57,14 @@
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
+                                <div class="bottom-links d-flex justify-content-between">
+                                    <p>New user?<a href="#">Register</a></p>
+                                    @if (Route::has('password.request'))
+                                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                                            {{ __('Forgot Your Password?') }}
+                                        </a>
+                                    @endif
+                                </div>
                             </div>
                         </div>
                     </form>
