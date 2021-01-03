614fbc5a520f359a2aba634993a7f4f585e5dd6a
Viral Prajapati <viral.prajapati@innvonix.com>
2021-01-03 17:32:29 +0530

    working on post job time functionality

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -42,6 +42,7 @@ class AppointmentController extends Controller
             $appointment_date = Carbon::createFromFormat('d/m/Y', $request->date)->format('Y-m-d');
 
             $message_text = "sent";
+            $param = [];
             if($request->has('appointment_id')){
                 $param[ "id" ] = $request->appointment_id;
                 $message_text = "updated";
@@ -49,17 +50,14 @@ class AppointmentController extends Controller
             
             $param2 = ["user_id" => $user_id, "name" => $request->name, "email" => Auth::user()->email, "time" => $request->time, "appointment_date" => $appointment_date,"description" => $request->description];
 
-            $appointment = Appointment::updateOrCreate(
-                $param,
-                $param2
-            );
+            $appointment = Appointment::create($param2);
 
             if($appointment){
 
                 DB::commit();
-                $message = 'Your '.$message_text.' has been sent successfully';
+                $message = 'Your appointment has been '.$message_text.' successfully';
                 $responseData['status'] = 1;
-                $responseData['redirect'] = route('community.appointments');
+                $responseData['redirect'] = route('appointment.index');
                 $responseData['message'] = $message;
                 Session::flash('success', $responseData['message']);
             } else {
@@ -75,6 +73,36 @@ class AppointmentController extends Controller
             $code = ($e->getCode() != '')?$e->getCode():500;
             $responseData['message'] = trans('common.something_went_wrong');
             return $this->commonResponse($responseData, $code);
+        }
+    }
+    public function detail(Request $request){
+        $responseData = array();
+        $responseData['status'] = 0;
+        $responseData['message'] = '';
+        $responseData['errors'] = array();
+        $responseData['data'] = [];
+
+        $appointment = Appointment::whereId($request->id)->first();
+        $responseData['data']['name'] = $appointment->name;
+        $responseData['data']['email'] = $appointment->user->email;
+        $responseData['data']['date'] = $appointment->appointment_date;
+        $responseData['data']['time'] = $appointment->time;
+        $responseData['data']['description'] = $appointment->description;
+        $responseData['status'] = 1;
+
+        return $this->commonResponse($responseData, 200);
+    }
+    public function destroy(Request $request){
+        DB::beginTransaction();
+        try{
+            $Appointment = Appointment::find($request->id);
+            $Appointment->delete();
+            DB::commit();
+            Session::flash('success', 'Appointment has been deleted Successfully');
+            return array('status' => '200', 'msg_success' => 'Appointment has been deleted Successfully');
+        } catch(Exception $e){
+            DB::rollback();
+            return array('status' => '0', 'msg_fail' => 'Something went wrong');
         }
     }
 }
