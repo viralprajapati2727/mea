5282ccf8f240fe7d084eab1eb1559184590e6380
nishaprajapatilive2727 <nishaprajapatilive2727@gmail.com>
2021-03-02 23:00:46 +0530

    responsive

--- resources/views/layouts/partial/footer.blade.php
+++ resources/views/layouts/partial/footer.blade.php
@@ -1,10 +1,10 @@
 <footer class="site-footer">
     <div class="container">
         <div class="row">
-            <div class="col-md-4">
+            <div class="col-sm-4 footer-col-wrap">
                 <div class="footer-col">
                     <h3 class="f-title">Company</h3>
-                    <ul class="footer-links">
+                    <ul class="footer-links"> 
                         <li><a href="{{ route('page.about-us') }}">About</a></li>
                         <li><a href="{{ route('page.team') }}">Team</a></li>
                         <li><a href="{{ route('page.contact-us') }}">Contact</a></li>
@@ -13,7 +13,7 @@
                     </ul>   
                 </div>
             </div>
-            <div class="col-md-4">
+            <div class="col-sm-4 footer-col-wrap"> 
                 <div class="footer-col">
                 <h3 class="f-title">Community</h3>
                     <ul class="footer-links">
@@ -23,7 +23,7 @@
                     </ul>     
                 </div>
             </div>
-            <div class="col-md-4">
+            <div class="col-sm-4 footer-col-wrap">
                 <div class="footer-col">
                     <h3 class="f-title">Register for MEA updates</h3>
                     <div class="newsletter-wrap">
