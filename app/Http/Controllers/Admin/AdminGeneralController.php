103cde034360af687230c66513d88c3ffabfffd2
Nisha Prajapati <nishap@accorin.com>
2021-09-06 22:43:06 +0530

    added approve button

--- app/Http/Controllers/Admin/AdminGeneralController.php
+++ app/Http/Controllers/Admin/AdminGeneralController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\SendMailController;
 use Illuminate\Http\Request;
 use App\User;
 use Carbon\Carbon;
@@ -28,15 +29,26 @@ class AdminGeneralController extends Controller
             DB::beginTransaction();
             $currentUser = User::where('id',$request->id)->first();
 
-            $currentUser = $currentUser->update([
-                'is_active' => $request->active,
-            ]);
+            $current_status = $currentUser->active;
+
+            $currentUser->is_active = $request->active;
+            $currentUser->save();
+            
             if($request->active == 2){
                 $msg = 'User has been Deactivated Successfully';
             }else {
                 $msg = 'User has been Activated Successfully';
             }
+            
             DB::commit();
+
+            if($current_status == 0){
+                SendMailController::dynamicEmail([
+                    'email_id' => 2,
+                    'user_id' => $currentUser->id,
+                ]);
+            }
+
             return array('status' => 200,'msg_success' => $msg,'active' => $request->active);
         } catch(Exception $e){
             \Log::info($e->getMessage());
