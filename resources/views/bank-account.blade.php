eebff4a36d24428ff84e06c59108926b2fca3dc7
Nikunj Sondagar <nikksondagar1696@gmail.com>
2021-10-05 23:12:15 +0530

    stripe registration refresh issue solved

--- resources/views/bank-account.blade.php
+++ resources/views/bank-account.blade.php
@@ -8,19 +8,23 @@
             <hr />
         </div>
         <div class="profile-top-detial">
-          @if ($stripeDetails)    
-            <div>
-              <strong>Bank Name :</strong> <span>{{ $stripeDetails->bank_name ?? '-' }}</span>
-            </div>
-            <div>
-              <strong>Account Number :</strong> <span>********{{ $stripeDetails->last4 ?? '-' }}</span>
-            </div>
-            <div>
-              <strong>Routing Number :</strong> <span>{{ $stripeDetails->routing_number ?? '-' }}</span>
-            </div>
-            <div>
-              <strong>Account Setup :</strong> <span>{{ $stripeDetails->account_status ?? '-' }}</span>
-            </div>
+          @if ($stripeDetails)
+            @if ($stripeDetails->details_submitted == 'true')                
+              <div>
+                <strong>Bank Name :</strong> <span>{{ $stripeDetails->bank_name ?? '-' }}</span>
+              </div>
+              <div>
+                <strong>Account Number :</strong> <span>********{{ $stripeDetails->last4 ?? '-' }}</span>
+              </div>
+              <div>
+                <strong>Routing Number :</strong> <span>{{ $stripeDetails->routing_number ?? '-' }}</span>
+              </div>
+              <div>
+                <strong>Account Setup :</strong> <span>{{ $stripeDetails->account_status ?? '-' }}</span>
+              </div>
+            @else
+              <a href="{{ route('create-account') }}" class="btn btn-primary">Completed Bank Details</a>              
+            @endif
           @else
               <a href="{{ route('create-account') }}" class="btn btn-primary">Add Bank Details</a>
           @endif
